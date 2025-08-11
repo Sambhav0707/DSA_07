adcd111c9e7e3d8dcad1c656f8f4358ca79d2e8a
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-08-11 16:45:21 +0530

    Time: 14 ms (72.73%), Space: 148.9 MB (100%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.dart
@@ -0,0 +1,46 @@
+class Solution {
+  int calTarget(List<int> piles , int hourlyRate){
+    int total = 0;
+    for(int i = 0 ; i< piles.length ; i++){
+        total += (piles[i] / hourlyRate ).ceil();
+
+    }
+    return total;
+  }  
+  int minEatingSpeed(List<int> piles, int hour) {
+    int maxx = 0;
+    for(int i = 0 ; i< piles.length ; i++){
+        maxx = max(maxx, piles[i]);
+    }
+
+    int ans = 0;
+
+//    for(int i = 1 ; i < maxx ; i++ ){
+//     int target = calTarget(piles , i);
+
+//     if(target <= h){
+//      ans = i;
+//      break;
+//     }
+//    }
+
+int l = 1 , h = maxx;
+
+while(l<= h){
+    int mid = (l+h) ~/ 2;
+
+    int target = calTarget(piles , mid);
+
+    if(target <= hour){
+        ans = mid;
+        h = mid - 1;
+    }else{
+        l = mid + 1;
+    }
+
+}
+
+   return ans;
+    
+  }
+}
