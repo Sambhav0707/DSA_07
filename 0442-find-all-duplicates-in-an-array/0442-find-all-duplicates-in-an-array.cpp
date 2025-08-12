15838dce0604d4a84162c6d3d9257407411da09a
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-08-12 09:53:16 +0530

    Time: 63 ms (11.18%), Space: 67.2 MB (17.03%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -15,14 +15,26 @@ public:
     // }
     // return ans;
 
-    vector<int>ans;
-    sort(nums.begin() , nums.end());
+    // vector<int>ans;
+    // sort(nums.begin() , nums.end());
 
-    for(int i = 0;i<nums.size() -1 ; i++){
-        if(nums[i] == nums[i+1]){
+    // for(int i = 0;i<nums.size() -1 ; i++){
+    //     if(nums[i] == nums[i+1]){
+    //         ans.push_back(nums[i]);
+    //     }
+
+    // }
+    // return ans;
+
+    unordered_map<int,int>mpp;
+    vector<int>ans;
+
+    for(int i = 0;i<nums.size() ; i++){
+         mpp[nums[i]]++;
+        if(mpp[nums[i]] == 2){
             ans.push_back(nums[i]);
         }
-
+       
     }
     return ans;
     }
