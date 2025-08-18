c37a4241bc9c95d742344f491e6f1808ac87402c
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-08-18 17:22:02 +0530

    Time: 1 ms (86.21%), Space: 146.8 MB (93.1%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.dart
@@ -0,0 +1,20 @@
+class Solution {
+  int maximum69Number (int num) {
+    String number = num.toString();
+    List<String> chars = number.split('');
+
+    for(int i = 0 ; i <chars.length ; i++){
+        if(chars[i] == '6'){
+            chars[i] = '9';
+            break;
+        }
+    }
+
+    String newNumber = chars.join('');
+
+
+    return int.parse(newNumber);
+
+    
+  }
+}
