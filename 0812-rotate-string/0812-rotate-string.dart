15532b9cf710af9f5e63f7fa0325505efd9975e8
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-08-19 12:06:45 +0530

    Time: 0 ms (100%), Space: 148.5 MB (52.17%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.dart
+++ 0812-rotate-string/0812-rotate-string.dart
@@ -1,15 +1,20 @@
 class Solution {
+  /* 
+  WE WILL MAKE A FUNCTION THAT GIVES US THE ROTATED STRING 
+  BY SHIFTING THE RIGHT MOST CHAR TO THE LEFT MOST CHAR
+   */  
   String convert(String s){
     return s.substring(1) + s[0];
   }
   bool rotateString(String s, String goal) {
-    String sub = s;
+
+    String sub = s; // WE THEN ASSIGN S TO A VARIABLE STRING 
     for(int i = 0 ; i<s.length ; i++){
 
-        sub = convert(sub);
+        sub = convert(sub); // WE ROTATE THE VARIABLE STRING
 
         if(sub == goal){
-            return true;
+            return true; // THEN WE CHECK FOR EACH ROTATION 
             break;
         }
        
