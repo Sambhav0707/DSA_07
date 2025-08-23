e9503ffaa0eac9c96eb04913776a1089a8edb861
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-08-23 10:52:25 +0530

    Time: 3 ms (43.99%), Space: 9.5 MB (8.96%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    /*
+    so the intuition is first we combine the two strings.
+    then we maintian an map and a set .
+    so the right pointer would start from the combined string s
+    and the left pointer would start fron the t.size() th position in the combined string s
+    now we check that s[l] key is present in the map ?
+      if it is present then we simply check that its value should we == to s[r]
+        if it is then => r++ , l++;
+        if it is not => false;
+      if it is not present then 
+        we check that the s[r] is used by checking that the s[r] is present in the set or not 
+        if it is => false;
+        if it is not then just add the s[l] => s[r] and in the set => insert s[r] ans l++ , r++;           
+    */
     bool isIsomorphic(string s, string t) {
         if(s.size() != t.size()){
             return false;
