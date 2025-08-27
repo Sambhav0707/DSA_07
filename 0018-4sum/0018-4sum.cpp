92d6d93cac6cd684b9cc31cf05e07fd88faed0b7
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-08-27 11:33:12 +0530

    Time: 14 ms (93.84%), Space: 17.6 MB (27.97%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -10,9 +10,9 @@ public:
         //     for(int j = i+1 ;j<n ; j++){
         //         for(int k = j+1; k < n ; k++){
         //             for(int l = k + 1 ; l< n; l++){
-        //                 long long sum = nums[i] + nums[j];
-        //                 sum += nums[k];
-        //                 sum += nums[l];
+                        // long long sum = nums[i] + nums[j];
+                        // sum += nums[k];
+                        // sum += nums[l];
         //                 if(sum == target){
         //                     vector<int> temp = {nums[i] , nums[j] ,nums[k] , nums[l]};
         //                     sort(temp.begin() , temp.end());
@@ -28,29 +28,72 @@ public:
         //  return ans;
 
         //BETTER 
-        set<vector<int>>sett;
-        for(int i = 0 ; i<n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                set<long long> hashset;
-                for(int k = j+1 ; k<n ; k++){
-                    long long sum = nums[i] + nums[j];
-                    sum += nums[k]; 
-                    long long l = target - (sum);
-                    auto it = hashset.find(l);
-                    if(it != hashset.end()){
-                        vector<int> temp = {nums[i] , nums[j] ,nums[k] , int(l)};
-                        sort(temp.begin() , temp.end());
-                        sett.insert(temp);
+        // set<vector<int>>sett;
+        // for(int i = 0 ; i<n ; i++){
+        //     for(int j = i + 1 ; j < n ; j++){
+        //         set<long long> hashset;
+        //         for(int k = j+1 ; k<n ; k++){
+        //             long long sum = nums[i] + nums[j];
+        //             sum += nums[k]; 
+        //             long long l = target - (sum);
+        //             auto it = hashset.find(l);
+        //             if(it != hashset.end()){
+                        // vector<int> temp = {nums[i] , nums[j] ,nums[k] , int(l)};
+        //                 sort(temp.begin() , temp.end());
+        //                 sett.insert(temp);
 
 
-                    }
-                    hashset.insert(nums[k]);
+        //             }
+        //             hashset.insert(nums[k]);
+        //         }
+        //     }
+        // }
+
+        //  vector<vector<int>> ans(sett.begin() , sett.end());
+        //  return ans;
+
+
+        //OPTIMAL 
+        sort(nums.begin() , nums.end());
+        vector<vector<int>> ans;
+
+        for(int i = 0 ; i< n ; i++){
+              if( i>0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j<n ; j++){
+                int k = j+1;
+                int l = n - 1;
+                
+              
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+
+                while(k<l){
+
+                        long long sum = nums[i] + nums[j];
+                        sum += nums[k];
+                        sum += nums[l];
+
+                        if(sum > target){
+                           
+                            l--;
+
+                        }
+                      else  if(sum < target){
+                        k++;
+                      }else{
+                         vector<int> temp = {nums[i] , nums[j] ,nums[k] , nums[l]};
+                            ans.push_back(temp);
+                            k++;
+                            l--;
+                            while(k<l && nums[k] == nums[k-1]) k++;
+                            while(k<l && nums[l] == nums[l+1]) l--;
+
+                      }
+
                 }
             }
         }
 
-         vector<vector<int>> ans(sett.begin() , sett.end());
-         return ans;
+        return ans;
 
         
     }
