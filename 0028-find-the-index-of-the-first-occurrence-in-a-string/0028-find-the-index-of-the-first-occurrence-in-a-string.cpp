0de06848c4394ef9c0224e05c06428eeec0e7101
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-09-01 18:32:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (57.66%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -15,16 +15,39 @@ public:
         // }
 
         // return -1;
+        
+        //BETTER
 
-        for(int i = 0 ; i < haystack.size() ; i++ ){
-            if(haystack[i] == needle[0]){
-                string s = haystack.substr(i , needle.size());
-                if(s == needle){
-                    return i;
-                    break;
-                }
+        // for(int i = 0 ; i < haystack.size() ; i++ ){
+        //     if(haystack[i] == needle[0]){
+        //         string s = haystack.substr(i , needle.size());
+        //         if(s == needle){
+        //             return i;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return -1;
+
+        //OPTIMAL
+        int l = 0 , r = 0;
+        while(l< haystack.size() && r < needle.size() ){
+            int start = l;
+            while(l< haystack.size() && r < needle.size() && haystack[l] == needle[r]){
+                l++;
+                r++;
+
             }
+
+            if(r== needle.size()){
+                return start;
+                break;
+            }
+
+            l = start +1;
+            r= 0;
         }
+
         return -1;
     }
 };
