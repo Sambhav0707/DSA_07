b62ad94b67f94048c40e8a2e8abca86a6e949281
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-09-16 11:07:45 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.85%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -12,34 +12,59 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         //BRUTE FORCE
+        // int count = 0;
+        // ListNode* temp = head;
+        // while(temp != nullptr){
+        //     count++;
+        //     temp = temp->next;
+        // }
+
+        // int res = count - n;
+
+        // //edge case :- if count == n; remove head
+
+        // if(count == n){
+        //     head = head->next;
+        //     return head;
+        // }
+
+        // temp = head;
+        // while(temp != nullptr){
+        //     res--;
+        //     if(res == 0){
+        //         temp->next = temp->next->next;
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+
+
+        // return head;
+
+
+        //OPTIMAL using fast and slow pointers
+
+        ListNode* fast = head;
+        ListNode* slow = head;
         int count = 0;
-        ListNode* temp = head;
-        while(temp != nullptr){
-            count++;
-            temp = temp->next;
+
+       for(int  i  = 0 ; i < n ; i++){
+            fast = fast->next;
+       }
+       
+  if (fast == nullptr)
+        return head->next;
+
+       while (fast->next != nullptr){
+           
+             fast = fast->next;
+             slow = slow->next;
         }
 
-        int res = count - n;
-
-        //edge case :- if count == n; remove head
-
-        if(count == n){
-            head = head->next;
-            return head;
-        }
-
-        temp = head;
-        while(temp != nullptr){
-            res--;
-            if(res == 0){
-                temp->next = temp->next->next;
-                break;
-            }
-            temp = temp->next;
-        }
-
+        slow->next = slow->next->next;
 
         return head;
+
 
      
         
