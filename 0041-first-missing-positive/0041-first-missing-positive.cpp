568af751497c49c329393c0d305d4b0d525cbfa8
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-09-29 12:06:21 +0530

    Time: 0 ms (100%), Space: 55 MB (48.7%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int , int>mp;
+        // unordered_map<int , int>mp;
 
-        for(int i  = 0; i < nums.size() ; i++){
-            mp[nums[i]]++;
-        }
+        // for(int i  = 0; i < nums.size() ; i++){
+        //     mp[nums[i]]++;
+        // }
 
-        int ans = 1;
+        // int ans = 1;
 
-        for(auto it : mp){
-            if(mp.find(ans) == mp.end()){
-                break;
-            }else{
-                ans++;
+        // for(auto it : mp){
+        //     if(mp.find(ans) == mp.end()){
+        //         break;
+        //     }else{
+        //         ans++;
+        //     }
+        // }
+
+        // return ans;
+
+        // Add this cleanup loop first
+for (int i = 0; i < nums.size(); ++i) {
+    if (nums[i] <= 0) {
+        nums[i] = nums.size() + 1; // Change to a number that's out of range
+    }
+}
+
+        for(int  i  = 0 ; i<nums.size() ; i++){
+         
+            int abs_val = abs(nums[i]);
+            if(abs_val <= nums.size()){
+
+                if(nums[abs_val - 1] > 0) nums[abs_val - 1] *= -1;
+
             }
         }
-
-        return ans;
+      
+        for(int  i  = 0 ; i<nums.size() ; i++ ){
+            if(nums[i] > 0){
+                return i + 1;
+            }
+        }
+         
+        return nums.size() + 1;
 
         
         
