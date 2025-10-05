72f589eccd113950975dce59f6bdbf04d393f2f3
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-10-05 15:25:47 +0530

    Time: 83 ms (26.44%), Space: 93.8 MB (74.34%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,54 +1,21 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        //BRUTE FORCE
-        // for(int i = 0 ; i < nums.size() ; i++){
-        //     for(int j = i+1 ; j < nums.size() ; j++){
-        //         if(nums[i] == nums[j]){
-        //             if(abs(i-j) <= k){
-        //                 return true;
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // }
+      
+      unordered_set<int>st;
 
-        // return false;
-
-        //BETTER
-        // unordered_map<int , int>mpp;
-        // for(int i = 0; i<nums.size() ; i++){
-            
-
-        //     auto it = mpp.find(nums[i]);
-        //     if(it != mpp.end()){
-        //        if( it->second != i){
-        //         if(abs(i - it->second) <= k){
-        //             return true;
-        //             break;
-        //         }
-        //        }
-        //     }
-        //     mpp[nums[i]] = i;
-        // }
-        // return false;
-
-        //OPTIMAL 
-         set<int>stt;
-
-        for (int i = 0; i < nums.size(); i++) {
-            
-            if (stt.count(nums[i])) {
-                return true;
-            }
-            stt.insert(nums[i]);
-            if (stt.size() > k) {
-                stt.erase(nums[i - k]);
-            }
+      for(int i = 0 ; i<nums.size() ; i++){
+        if(st.size() > k){
+            st.erase(nums[i-k-1]);
         }
 
-        return false;
+        if(st.count(nums[i])){
+            return true;
+        }
 
-        
+        st.insert(nums[i]);
+      }
+
+      return false;
     }
 };
