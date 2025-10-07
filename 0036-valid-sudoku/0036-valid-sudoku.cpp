770cdb968296a2106102f0f7c0345e271a2fd119
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-10-07 21:37:22 +0530

    Time: 4 ms (41.09%), Space: 24.9 MB (27%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        unordered_map<int,set<int>>rows;
+        unordered_map<int,set<int>>cols;
+        map<pair<int,int> , set<int>>sq;
+
+        for(int i = 0 ; i < 9 ; i++){
+            for(int j = 0 ; j<9 ; j++){
+                if(board[i][j] == '.') continue;
+                if(rows[i].count(board[i][j]) || cols[j].count(board[i][j]) || sq[{i/3, j/3}].count(board[i][j])) return false;
+               rows[i].insert(board[i][j]);
+               cols[j].insert(board[i][j]);
+               sq[{i/3,j/3}].insert(board[i][j]);
+
+            }
+        }
+
+        return true;
+
+        
+    }
+};
