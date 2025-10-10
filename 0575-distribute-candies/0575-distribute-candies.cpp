7af03b169de5ce4ada7fb3023f21f915d8239225
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-10-10 23:10:05 +0530

    Time: 108 ms (54.43%), Space: 120 MB (71.2%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+
+        int  n = candyType.size() / 2;
+
+        unordered_map<int,int>mp;
+
+        for(int c : candyType ){
+            mp[c]++;
+        }
+
+        if(mp.size() > n) return n;
+
+        return mp.size();
+        
+    }
+};
