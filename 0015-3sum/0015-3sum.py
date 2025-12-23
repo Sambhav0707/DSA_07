e8e7259135a74938da1323e6835e544f9d375e94
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-12-23 15:44:12 +0530

    Time: 400 ms (96.07%), Space: 20.2 MB (94.34%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -15,24 +15,58 @@ class Solution:
 
         # return result
 
-        result = []
-        finalset = set()
-        for i in range(len(nums)):
-            hashset = set()
-            for j in range(i + 1 ,len(nums)):
-                third = -(nums[i] + nums[j])
+        #Better:-  
 
-                if third in hashset:
-                    triplet = [nums[i] , nums[j] , third]
-                    triplet.sort()
-                    if tuple(triplet) not in finalset:
-                        finalset.add(tuple(triplet))
-                        result.append(triplet)
+        # result = []
+        # finalset = set()
+        # for i in range(len(nums)):
+        #     hashset = set()
+        #     for j in range(i + 1 ,len(nums)):
+        #         third = -(nums[i] + nums[j])
+
+        #         if third in hashset:
+        #             triplet = [nums[i] , nums[j] , third]
+        #             triplet.sort()
+        #             if tuple(triplet) not in finalset:
+        #                 finalset.add(tuple(triplet))
+        #                 result.append(triplet)
                         
+        #         else:
+        #             hashset.add(nums[j])
+
+
+        # return result
+
+        result =[]
+        nums.sort()
+
+        for index , value in enumerate(nums):
+            if value > 0:
+                break
+            
+            if index > 0 and value == nums[index - 1]:
+                continue
+            
+            l , r = index + 1 , len(nums) - 1
+
+            while l < r:
+                third = value + nums[l] + nums[r]
+
+                if third > 0:
+                    r -= 1
+                elif third < 0:
+                    l += 1
                 else:
-                    hashset.add(nums[j])
+                    result.append([value , nums[l] , nums[r]])
 
+                    l+= 1
+                    r -= 1
 
+                    while nums[l] == nums[l - 1 ] and l < r:
+                        l += 1
+        
         return result
+
+
 
         
