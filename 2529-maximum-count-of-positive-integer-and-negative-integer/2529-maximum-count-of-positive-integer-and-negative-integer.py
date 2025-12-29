809fed265afd0bdce7c75eb326e6d55607268f10
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2025-12-29 12:12:21 +0530

    Time: 0 ms (100%), Space: 17.9 MB (59.15%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,16 +1,61 @@
 class Solution:
-    def maximumCount(self, nums: List[int]) -> int:
-        count_n = 0
-        count_p = 0
+    def upper_bound(self , nums: List[int]) -> int:
+        l = 0
+        h = len(nums) - 1
+        ans = -1
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                continue
+        while l <= h:
+            mid = (l + h) // 2
+
+            if nums[mid] > 0:
+                ans = mid
+                h = mid -1
             
-            if nums[i] > 0:
-                count_p += 1
             else:
-                count_n += 1
+                l = mid + 1
+            
+        return ans
+    
+    def lower_bound(self , nums: List[int]) -> int:
+        l = 0
+        h = len(nums) - 1
+        ans = -1
+
+        while l <= h:
+            mid = (l + h) // 2
+
+            if nums[mid] < 0:
+                ans = mid
+                l = mid + 1
+            else:
+                h = mid - 1
+            
+            
+            
+        return ans
+
+    def maximumCount(self, nums: List[int]) -> int:
+        # count_n = 0
+        # count_p = 0
+
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         continue  
+        #     if nums[i] > 0:
+        #         count_p += 1
+        #     else:
+        #         count_n += 1
         
+        # return max(count_p , count_n)
+
+        index_n = self.lower_bound(nums)
+        index_p = self.upper_bound(nums) 
+
+        count_n =  index_n + 1 if index_n != -1 else 0
+        count_p = len(nums) - index_p if index_p != -1 else 0
         return max(count_p , count_n)
+
+
+
+        
         
