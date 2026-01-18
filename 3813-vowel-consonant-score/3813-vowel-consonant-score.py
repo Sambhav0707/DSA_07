87b7bed2b4a41cf25cbc47793338cbc0732e5883
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-01-18 14:36:39 +0530

    Time: 3 ms (39.46%), Space: 19.5 MB (52.44%) - LeetHub

--- 3813-vowel-consonant-score/3813-vowel-consonant-score.py
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.py
@@ -1,15 +1,17 @@
 class Solution:
     def vowelConsonantScore(self, s: str) -> int:
         vowels = ['a' , 'e' , 'i' , 'o' , 'u']
+        digits = ['1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0']
         vc = 0
         cc = 0
         for c in s:
             if c in vowels:
                 vc += 1
+            elif c in digits or c == ' ' :
+                continue
             else:
-                if not c.isdigit() and not c.isspace():
-                    cc += 1
-
+                cc += 1
+                    
         if cc == 0:
             return 0
         
