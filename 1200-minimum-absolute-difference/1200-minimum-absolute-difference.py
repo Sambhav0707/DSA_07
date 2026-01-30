7818bc3e5ac0f292a8eb1d16951f090da5816ec1
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-01-30 15:20:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -1,20 +1,41 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        # arr.sort()
+        # min_abs = float("inf")
+        # l = 0
+        # for r in range(1,len(arr)):
+        #     diff = abs(arr[l] - arr[r])
+        #     min_abs = min(min_abs , diff)
+        #     l = r
+        
+        # l = 0
+        # ans=[]
+        # for r in range(1,len(arr)):
+        #     diff = abs(arr[l] - arr[r])
+        #     if diff == min_abs:
+        #         ans.append([arr[l] , arr[r]])
+        #     l = r
+        
+        # return ans
+
+
         arr.sort()
         min_abs = float("inf")
         l = 0
+        ans = []
         for r in range(1,len(arr)):
             diff = abs(arr[l] - arr[r])
-            min_abs = min(min_abs , diff)
-            l = r
-        
-        l = 0
-        ans=[]
-        for r in range(1,len(arr)):
-            diff = abs(arr[l] - arr[r])
+
             if diff == min_abs:
                 ans.append([arr[l] , arr[r]])
+            if diff < min_abs:
+                ans.append([arr[l] , arr[r]])
+                min_abs = diff
+            
             l = r
+
+            
+            
         
         return ans
 
