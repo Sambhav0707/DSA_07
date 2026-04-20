8c9e8bedc4c9325c84dade8fd9ae8dec372696ec
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-04-20 13:42:12 +0530

    Time: 1217 ms (7.73%), Space: 23.8 MB (22.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,19 +2,29 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
 
-        dp = [[float('inf') for _ in range(amount + 1)] for _ in range(n + 1)]
+        dp = [[0 for _ in range(amount + 1)] for _ in range(n + 1)]
 
-        for i in range(n+1):
-            dp[i][0] = 0
+        for i in range(n + 1):
+            for j in range(amount + 1):
+                if i == 0:
+                    dp[i][j] = float("inf") - 1
+                elif i > 0 and j == 0:
+                    dp[i][j] = 0
+                elif i == 1 and j > 0:
+                    if  j % coins[0] == 0:
+                        dp[i][j] = j // coins[0]
+                    else:
+                        dp[i][j] = float("inf") - 1
+        
 
-        for i in range(1, n+1):
-            for j in range(1, amount + 1):
-                if coins[ i - 1] <= j:
-                    dp[i][j] = min(1 + dp[i][j - coins[i-1]] , dp[i-1][j])
+        for i in range(2, n + 1):
+            for j in range(1 , amount + 1):
+                if coins[i-1] <= j:
+                    dp[i][j] = min(1+dp[i][j - coins[i-1]] , dp[i-1][j])
                 else:
                     dp[i][j] = dp[i-1][j]
-
-        if dp[n][amount] == float("inf"):
+        
+        if dp[n][amount] == float("inf") - 1:
             return -1
-        return dp[n][amount] 
+        return dp[n][amount]
         
