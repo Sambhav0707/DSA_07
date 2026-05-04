1a259ebad1abaf4571faedbb9ab50a258eefabb2
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-05-05 00:21:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -2,13 +2,14 @@ class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         prefix_sum = 0
 
-        remainder = {0:-1}
+        remainder = {}
 
         for i in range(len(nums)):
             prefix_sum += nums[i]
 
             r = prefix_sum % k 
-
+            if r == 0 and i >=1:
+                return True
             if r in remainder:
                 if i - remainder[r] >= 2:
                     return True
