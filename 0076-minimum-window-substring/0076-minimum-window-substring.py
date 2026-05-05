056ebc5c8ae0441f9598ad274b6d43cc9ea2c65d
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-05-05 18:02:49 +0530

    Time: 131 ms (27.87%), Space: 19.6 MB (74.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,76 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # if len(t) > len(s):
-        #     return ""
-        # result = float("inf")
-        # mp = {}
-        # ans = ""
-
-        # for i in range(len(t)):
-        #     mp[t[i]] = 1 + mp.get(t[i] , 0)
-        
-        
-
-        # for i in range(len(s)):
-        #     mp2 = mp.copy()
-        #     for j in range(i , len(s)):
-        #         if s[j] in mp2:
-        #             mp2[s[j]] -= 1
-
-        #             if mp2[s[j]] == 0:
-        #                 mp2.pop(s[j])
-                
-        #         if len(mp2) == 0:
-        #             if (j - i + 1) < result:
-        #                 result = j - i + 1
-        #                 ans = s[i:j + 1]
-                     
-        
-        # return ans
-
-        # optimise solution :- TC:- O(m + n) and SC:- O(1)
-
-        countT , currentWindow = {} , {}
-
-        for c in t:
-            countT[c] = 1 + countT.get(c , 0)
-        
-        have , need = 0 , len(countT)
-        res , resLen = [-1,-1] , float("inf")
-
+        # variables declarations
         l = 0
+        res = ""
+        res_len = float("inf")
+        have = 0
+        window = defaultdict(int)
+        t_char_need_dict = Counter(t)
+        t_char_need = len(t_char_need_dict)
 
+        # Sliding window logic
         for r in range(len(s)):
-            c = s[r]
+            window[s[r]] += 1
 
-            currentWindow[c] = 1 + currentWindow.get(c , 0)
-
-            if c in countT and currentWindow[c] == countT[c]:
+            if s[r] in t_char_need_dict and window[s[r]] == t_char_need_dict[s[r]]:
                 have += 1
             
-            while have == need:
-                if (r - l + 1) < resLen:
-                    resLen = r - l + 1
-                    res = [l , r]
+            while have == t_char_need:
+                if res_len > r-l+1:
+                    res = s[l:r+1]
+                    res_len = len(res)
                 
+                window[s[l]] -= 1
 
-                currentWindow[s[l]] -= 1
-
-                if s[l] in countT and currentWindow[s[l]] < countT[s[l]]:
+                if s[l] in t_char_need_dict and window[s[l]] < t_char_need_dict[s[l]]:
                     have -= 1
                 
-                l += 1 
-            
-        l , r = res
-
-        return s[l : r + 1] if resLen != float("inf") else ""
-            
-
-
-            
-
+                l += 1
         
+        return res
 
-
+            
 
         
