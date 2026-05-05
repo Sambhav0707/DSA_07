424f7537f37aa731207291f77982d5a3ed35d8ca
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-05-05 18:20:07 +0530

    Time: 16 ms (68.34%), Space: 19.4 MB (54.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,81 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # Brute force0
-        # result_tuple = permutations(s1)
-
-        # for res in result_tuple:
-        #     string = "".join(res)
-
-        #     if string in s2:
-        #         return True
-
-        # return False
-
-        #  TC :- O(26 * n)
-        # if len(s1) > len(s2):
-        #     return False
-
-        # mp = {}
-
-        # for ch in s1:
-        #     mp[ch] = 1 + mp.get(ch , 0)
-
-        # mp2 = {}
-
-        # l = 0
-        # r = len(s1) - 1
-
-        # for i in range(l , r + 1):
-        #     mp2[s2[i]] = 1 + mp2.get(s2[i] , 0)
-
-        # while r < len(s2):
-        #     if mp == mp2:
-        #         return True
-        #     mp2[s2[l]] -= 1
-        #     if mp2[s2[l]] <= 0:
-        #         mp2.pop(s2[l])
-
-        #     l += 1
-        #     r += 1
-        #     if r < len(s2):
-        #         mp2[s2[r]] = 1 + mp2.get(s2[r] , 0)
-
-        # if mp == mp2:
-        #     return True
-        # return False
-        # return False
-
-        #  TC :- O(26) + O(n)
-
-        if len(s1) > len(s2):
+        if len(s2) < len(s1):
             return False
-
-        s1Count, s2Count = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord("a")] += 1
-            s2Count[ord(s2[i]) - ord("a")] += 1
-
-        matches = 0
-        for i in range(26):
-            matches += 1 if s1Count[i] == s2Count[i] else 0
-
+        
+        window_size = len(s1)
+        s1_char_dict = Counter(s1)
+        current_window_dict = defaultdict(int) 
         l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
+
+        for r in range(len(s2)):
+            current_window_dict[s2[r]] += 1
+
+            while r-l+1 > window_size:
+                current_window_dict[s2[l]] -= 1
+
+                if current_window_dict[s2[l]] < 1:
+                    del current_window_dict[s2[l]]
+                
+                l += 1
+
+
+            if current_window_dict == s1_char_dict:
                 return True
+        
+        return False
+            
+        
 
-            index = ord(s2[r]) - ord("a")
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
 
-            index = ord(s2[l]) - ord("a")
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1
-            l += 1
-        return matches == 26
+        
