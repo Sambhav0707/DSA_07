7c873d84060ac4f41f0b79a03c477f5219b4e907
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-05-12 13:52:23 +0530

    Time: 48 ms (80.2%), Space: 55.5 MB (60.4%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -1,25 +1,40 @@
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
-        # sorting the tasks based on the difference of min - actual 
-        # so that we can process the task having larger difference first and then the smaller difference
-        tasks.sort(key=lambda x: x[1] - x[0])
+        """
+        Sorting the task based on the diff -> minimum - actual
 
-        # getting the starting min balance and total loan 
-        IMB = tasks[-1][1]
-        SMB = tasks[-1][1]
-        TL = 0
-        for i in range(len(tasks) - 1, -1, -1):
-            if SMB < tasks[i][1]:
-                # take loan 
-                TL += tasks[i][1] - SMB
-                # PROCESS THE TASK WITH New SMB
-                SMB += tasks[i][1] - SMB
-                SMB = SMB - tasks[i][0]
-            else:
-                SMB = SMB - tasks[i][0]
+        Why ? 
+
+        lets say Task A - [3 , 15] and Task B - [8,10]
+        SO if we process Task B -> starting minimum Energy -> 10 and after process -> energy left = 2 (10 - 8)
+        now we go to the Task A -> we need deficit of 13 (15 - 2) more energy to process 
+        So in the end the minimum energy required will be: 
+         initial start minimum engergy + deficit = 10 + 13 = 23
         
-        return IMB + TL
+        Now we do the Task A first which is having the diff (minimum - actual) greater
+        so the starting minimum energy is 15 and after processing the energy remains to 12 (15 - 3)
+        now to process Task B we dont need extra energy as we already have 13 left with us 
 
+        so the minimum starting energy = 15
+        """
+        tasks.sort(key=lambda x: x[1] - x[0] , reverse=True)
+
+        # getting the starting min balance and total deficit / total extra engery  
+        total_extra_energy = 0
+        present_energy = tasks[0][1] 
+
+        for task in tasks:
+            if present_energy < task[1]: # less then the minimum energy required
+                # updating the total extra energy needed
+                total_extra_energy += task[1] - present_energy
+                # present energy becomes the req min energy to begin the task
+                present_energy = task[1] 
+
+            # after the process setting up the energy after the process
+            present_energy -= task[0]
+        
+        return total_extra_energy + tasks[0][1]
+       
 
 
         
