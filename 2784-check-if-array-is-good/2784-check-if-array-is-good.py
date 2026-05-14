29d154a0f7344464854309a5c6023c1e36fbafbc
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-05-14 12:19:50 +0530

    Time: 0 ms (100%), Space: 19.2 MB (59.43%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        length = len(nums)
+        # finding the 'n' 
+        nums.sort()
+        n = nums[-1]
+        # checking the edge case where nums length is greater then base[n] elements
+        if n + 1 > length:
+            return False
+        
+        for i in range(length - 1 , -1 , -1):
+            if i == length - 1 or i == length - 2:
+                if nums[i] == n:
+                    continue
+                else:
+                    return False
+            
+            if nums[i] == n - 1:
+                n -= 1
+            else:
+                return False
+        
+        return True
+        
+        
+
+
+        
+
+        
