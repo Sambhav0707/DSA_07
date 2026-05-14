6aa9e3fb49ec049e4da07ac50abfc80641ed0e1b
sambhav dalal <146570003+Sambhav0707@users.noreply.github.com>
2026-05-14 12:22:11 +0530

    Time: 4 ms (11.03%), Space: 19.2 MB (89.68%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,28 +1,33 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        length = len(nums)
-        # finding the 'n' 
+        # length = len(nums)
+        # # finding the 'n' 
+        # nums.sort()
+        # n = nums[-1]
+        # # checking the edge case where nums length is less then base[n] elements
+        # if n + 1 > length:
+        #     return False
+        # # traversing from last element and checking the element == n-1 / element == n
+        # for i in range(length - 1 , -1 , -1):
+        #     if i == length - 1 or i == length - 2:
+        #         if nums[i] == n:
+        #             continue
+        #         else:
+        #             return False
+            
+        #     if nums[i] == n - 1:
+        #         n -= 1
+        #     else:
+        #         return False
+        
+        # return True
+        
         nums.sort()
         n = nums[-1]
-        # checking the edge case where nums length is greater then base[n] elements
-        if n + 1 > length:
-            return False
-        
-        for i in range(length - 1 , -1 , -1):
-            if i == length - 1 or i == length - 2:
-                if nums[i] == n:
-                    continue
-                else:
-                    return False
-            
-            if nums[i] == n - 1:
-                n -= 1
-            else:
-                return False
-        
-        return True
-        
-        
+
+        expected = list(range(1, n)) + [n, n]
+
+        return nums == expected
 
 
         
